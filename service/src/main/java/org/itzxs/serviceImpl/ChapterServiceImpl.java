8d9cb1b5c112b94aca57661298ccd337fe657e65
zhangxinshuai <zhangxinshuai@maxrocky.com>
2018-06-20 14:46:41 +0800

    爬取章节更改、

--- service/src/main/java/org/itzxs/serviceImpl/ChapterServiceImpl.java
+++ service/src/main/java/org/itzxs/serviceImpl/ChapterServiceImpl.java
@@ -31,6 +31,8 @@ public class ChapterServiceImpl implements ChapterService{
 
     @Override
     public boolean updateChapter(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        System.out.println(simpleDateFormat.format(new Date()));
         List<Book> books = bookMapper.selectAllBook();
         List<Chapter> chapters = new ArrayList<>();
         for (Book book:books) {
@@ -39,30 +41,42 @@ public class ChapterServiceImpl implements ChapterService{
             chapters.addAll(chaptersByNovel);
         }
         System.out.println("爬取完毕");
-//        chapterMapper.insertChapters(chapters);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        System.out.println("=============================开始插入=================="+simpleDateFormat.format(new Date()));
-        //10个线程
-        ExecutorService executor = Executors.newFixedThreadPool(10);
-        //一组500
-        int MAX_DEAL = 500;
-        int times = (chapters.size() + 1 + MAX_DEAL) / MAX_DEAL;
-        //一个同步辅助类，在完成一组正在其他线程中执行的操作之前，它允许一个或多个线程一直等待。
-        CountDownLatch countDownLatch = new CountDownLatch(times);
-        try {
-            for (int i = 0; i < times; i++) {
-                System.out.println("开始批量插入"+i);
-                if (i == times - 1) {
-                    executor.execute(new UpdateChapterRunable(chapters.subList(i * MAX_DEAL, chapters.size()), countDownLatch));//调用业务逻辑
-                } else {
-                    executor.execute(new UpdateChapterRunable(chapters.subList(i * MAX_DEAL, (i + 1) * MAX_DEAL), countDownLatch));
-                }
-            }
-            System.out.println("=============================结束插入=================="+simpleDateFormat.format(new Date()));
-            countDownLatch.await();//一个线程(或者多个)， 等待另外N个线程完成某个事情之后才能执行
-        } catch (Exception e) {
-            e.printStackTrace();
+        System.out.println(simpleDateFormat.format(new Date()));
+        int chapterSize = chapters.size();
+        int range = 50;
+        int loopTimes = chapterSize / range;
+        int lastIndex = chapterSize-1;
+        for (int i = 0;i< loopTimes;i++){
+            int startIndex = loopTimes * i;
+            int endIndex = startIndex +range;
+            List<Chapter> insertChapters =
+            (i == loopTimes && endIndex!= lastIndex)? chapters.subList(startIndex, lastIndex):chapters.subList(startIndex, endIndex);
+            chapterMapper.insertChapters(insertChapters);
         }
+
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+//        System.out.println("=============================开始插入=================="+simpleDateFormat.format(new Date()));
+//        //10个线程
+//        ExecutorService executor = Executors.newFixedThreadPool(10);
+//        //一组500
+//        int MAX_DEAL = 500;
+//        int times = (chapters.size() + 1 + MAX_DEAL) / MAX_DEAL;
+//        //一个同步辅助类，在完成一组正在其他线程中执行的操作之前，它允许一个或多个线程一直等待。
+//        CountDownLatch countDownLatch = new CountDownLatch(times);
+//        try {
+//            for (int i = 0; i < times; i++) {
+//                System.out.println("开始批量插入"+i);
+//                if (i == times - 1) {
+//                    executor.execute(new UpdateChapterRunable(chapters.subList(i * MAX_DEAL, chapters.size()), countDownLatch));//调用业务逻辑
+//                } else {
+//                    executor.execute(new UpdateChapterRunable(chapters.subList(i * MAX_DEAL, (i + 1) * MAX_DEAL), countDownLatch));
+//                }
+//            }
+//            System.out.println("=============================结束插入=================="+simpleDateFormat.format(new Date()));
+//            countDownLatch.await();//一个线程(或者多个)， 等待另外N个线程完成某个事情之后才能执行
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         return true;
     }
 
